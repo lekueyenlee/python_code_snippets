11dea944263ebcd2513f7709354b4e7c0c64db8a
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-28 17:09:11 -0500

    Create WPE_ExampleA1.py
    Created a new code file for the coding exercise form weekly Python exercise

---
+++ WPE_ExampleA1.py
@@ -0,0 +1,9 @@
+'''  Coding Exercise from Weekly Python Exercise
+Getting input from the user
+"while" loops
+Converting data from strings to integers
+Working with lists
+The built-in "sum" function
+The built-in "len" function
+
+'''
