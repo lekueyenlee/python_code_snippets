08524583d88ae3f21ed98dff3d02f6248c6780c2
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-28 17:41:43 -0500

    Update WPE_ExampleA1.py
    Added in my solution in the example

--- WPE_ExampleA1.py
+++ WPE_ExampleA1.py
@@ -7,6 +7,27 @@ The built-in "sum" function
 The built-in "len" function
 '''
 
-user_input = input("Enter a number :")
+import sys
 
-print("You've entered :" + user_input)
+list_numbers = []
+
+user_input = input("Enter a number : ")
+
+
+def get_output(in_list): 
+    print("Sum of integers: " + str(sum(in_list)))
+    print("Average value: " + str(sum(in_list)/len(in_list)))
+    print("Max value: " + str(max(in_list)))
+    print("Min value: " + str(min(in_list)))
+
+
+while (user_input != ''): 
+    list_numbers.append(int(user_input))
+    user_input = input("Enter a number : ")
+
+get_output(list_numbers)
+
+
+sys.exit()
+
+
