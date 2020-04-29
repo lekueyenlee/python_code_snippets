bd8ff2e5de34d6d0e9eed4a3374b6578731fb8d6
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-28 22:36:52 -0500

    Create solution_wpe_1.py
    Added in solution for weekly python exercise 1

---
+++ solution_wpe_1.py
@@ -0,0 +1,27 @@
+
+def print_solutions():
+    all_numbers = []
+    smallest = None
+    largest = None
+
+    while True:
+        n = input("Enter a number: ").strip()
+
+        if not n: #if not true which means not empty, it exits
+            break
+
+        n = int(n)
+        all_numbers.append(n)
+
+        if (smallest is None or n < smallest):
+            smallest = n
+
+        if (largest is None or n > largest):
+            largest = n
+
+    print(f"Smallest: {smallest}")
+    print(f"Largest: {largest}")
+    print(f"Sum: {sum(all_numbers)}")
+    print(f"Mean: {sum(all_numbers ) / len(all_numbers)}")
+
+print_solutions()
